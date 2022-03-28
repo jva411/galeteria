a51438b83084b5304cb14475da9f2c88a6f00577
jva411 <j.victoralves411@gmail.com>
2022-03-28 19:07:50 -0300

    Update to get ipv4

--- front/lib/axiosConfig.js
+++ front/lib/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+const ipv4 = process.env.IPv4
+
 const instance = axios.create({
-    baseURL: 'http://192.168.2.142:5001'
+    baseURL: `http://${ipv4}:5001`
 });
 
 export default instance;
